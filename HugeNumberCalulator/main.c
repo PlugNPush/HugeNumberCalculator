217ed880a9a196be72b4595144ab248fe12f9d38
PlugNCS <michael.nass@free.fr>
2020-10-05 19:08:50 +0200

    Initial Commit

---
+++ HugeNumberCalulator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  HugeNumberCalulator
+//
+//  Created by PlugN on 5.10.20..
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
